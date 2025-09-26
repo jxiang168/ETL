dee0a57cde08ff170fb0825c3e02e6843894b559
jxiang168 <jxiang168@163.com>
2025-09-26 23:18:08 +0800

    Create run_on_win32.py

---
+++ Python/Playwright/run_on_win32.py
@@ -0,0 +1,17 @@
+import re
+from playwright.sync_api import Page, expect
+
+def has_title(page: Page):
+    page.goto("https://google.com/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwr"))
+
+def get_started_link(page: Page):
+    page.goto("https://youtube/")
+
+    # Click the get started link.
+    page.get_by_role("heading", name="Get started again").click()
+
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("link", name="Try not to Installation")).to_be_visible()
