34390041104757c7f700bd52340228d5ae2d908c
jxiang168 <jxiang168@163.com>
2026-01-01 19:38:30 +0800

    Create run_on_redhat.py

---
+++ Python/Playwright/run_on_redhat.py
@@ -0,0 +1,20 @@
+import re
+from playwright.sync_api import Page, expect
+
+def has_title(page: Page):
+    page.goto("https://google.com.uk/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwr"))
+
+    name = "jxiang168"
+    print(f"my name is {name}")
+
+def get_started_link(page: Page):
+    page.goto("https://youtube.com.uk/")
+
+    # Click the get started link.
+    page.get_by_role("heading", name="Get started again").click()
+
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("link", name="Try not to Installation")).to_be_visible()
